d2eafc87728522d72341f5fade8bfb7e6272d320
Oszvaldb <nagyongonoszi@gmail.com>
2017-02-17 10:02:10 +0100

    now works if the two vectors' size are different

--- main.cpp
+++ main.cpp
@@ -6,8 +6,14 @@ using namespace std;
 //Create a function that takes two arrays and mixes them like this: [1, 2, 3] and [6, 7, 8] becomes [1, 6, 2, 7, 3, 8]
 
 vector<int> unite_vectors(vector<int> vector1, vector<int> vector2) {
+  int biggersize;
+  if(vector1.size() > vector2.size()) {
+    biggersize = vector1.size();
+  } else {
+    biggersize = vector2.size();
+    }
   vector<int> united;
-  for(int i = 0; i < vector1.size(); i++) {
+  for(int i = 0; i < biggersize; i++) {
     united.push_back(vector1[i]);
     united.push_back(vector2[i]);
   }
